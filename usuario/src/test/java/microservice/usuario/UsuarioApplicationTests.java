8b137a098e54233d3d4de0f7aa9d814e6878404b
Matias Guerra <mat.guerraj@duocuc.cl>
2026-06-12 01:59:32 -0400

    Pruebas Unitarias listas

--- usuario/src/test/java/microservice/usuario/UsuarioApplicationTests.java
+++ usuario/src/test/java/microservice/usuario/UsuarioApplicationTests.java
@@ -1,6 +1,9 @@
 package microservice.usuario;
 
 import org.junit.jupiter.api.Test;
+import org.mockito.MockedStatic;
+import org.mockito.Mockito;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
@@ -8,6 +11,17 @@ class UsuarioApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	void mainRunsSpringApplication() {
+		String[] args = {};
+
+		try (MockedStatic<SpringApplication> springApplication = Mockito.mockStatic(SpringApplication.class)) {
+			UsuarioApplication.main(args);
+
+			springApplication.verify(() -> SpringApplication.run(UsuarioApplication.class, args));
+		}
 	}
 
 }
